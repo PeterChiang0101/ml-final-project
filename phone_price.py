5521cd1331c74c66bb6679c6e160438d271d14e0
Peter Chiang <peterchiang0119@gmail.com>
2022-12-05 18:50:11 +0800

    phone_price_predict complete

--- phone_price.py
+++ phone_price.py
@@ -11,7 +11,18 @@ high_corr = corr.index[abs(corr['price_range'])>0.1]
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-X = df[high_corr]
+X = df[high_corr.drop('price_range')]
 y = df['price_range']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+
 rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+
+prediction = rf.predict(X_test)
+
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
+print('Accuracy: ', accuracy_score(y_test, prediction))
+print('Recall: ', recall_score(y_test, prediction, average='macro'))
+print('Precision: ', precision_score(y_test, prediction, average='macro'))
